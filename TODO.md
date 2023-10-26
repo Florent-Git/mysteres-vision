775d0d3d6596d3fff5090a201cbb7eb28ff9bdc7
rafuryc <rafuryc@hotmail.be>
2023-10-26 16:28:25 +0200

    Fin du 26/10

---
+++ TODO.md
@@ -0,0 +1,9 @@
+Pour le next time :
+- Trouver les coordonnées de chaque tuile 
+- Lier la position de chaque tuile à son identifiant
+```json
+  {
+    "identifiant" : [averageIMG1, averageIMG2,…]
+  }
+```
+- Trouver la moyenne et l'écart-type de chaque tuile pour faire la détection des tuiles
--- custom_functions.py
+++ custom_functions.py
@@ -15,16 +15,25 @@ def reconstruct_image(base_image, mask, iterations=1, kernel=np.ones((3, 3))):
 
 
 def get_circles(image):
-    resized_image = cv.resize(image, (image.shape[1] // 2, image.shape[0] // 2))
-    grayed_image = cv.cvtColor(resized_image, cv.COLOR_RGB2GRAY)
+    grayed_image = cv.cvtColor(image, cv.COLOR_RGB2GRAY)
     blurred_image = cv.medianBlur(grayed_image, 5)
     _, threshold_image = cv.threshold(blurred_image, 70, 255, cv.THRESH_BINARY)
     closed_image = cv.morphologyEx(threshold_image, cv.MORPH_CLOSE, np.ones((31, 31)))
     image_circles = cv.HoughCircles(closed_image, cv.HOUGH_GRADIENT, dp=1, minDist=100, param1=50, param2=10, minRadius=73, maxRadius=78)
-
-    #print(image_circles)
-
     return np.uint16(np.around(image_circles))[0,:]
+
+
+def get_average_circles(image, circles):
+    circles_dict = {}
+    h,s,v = cv.split(cv.cvtColor(image, cv.COLOR_RGB2HSV))
+    for i in circles:
+        mask = np.full((image.shape[0], image.shape[1]),0,dtype=np.uint8)
+        cv.circle(mask, (i[0], i[1]), i[2], (255, 255, 255), -1)
+        circle_masked = cv.bitwise_and(h, h, mask = mask)
+        posCircle = circle_masked[circle_masked!=0]
+        circle_average = np.average(posCircle)
+        circles_dict[circle_average] = i
+    return circles_dict
 
 
 if __name__ == "__main__":
